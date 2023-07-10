0ce0318ea76092b05639192a413cf1d5dd312e3f
Anushareddy239 <Anusha.Reddy@triconinfotech.com>
2023-07-10 15:52:28 +0530

    RELEASE:test all

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,50 @@
 import { test, expect } from '@playwright/test';
 import * as login from '../res/scout.json';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link .
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
- 
+test.describe('Example-one [Main]', () => {
+  test('Example-one test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+  
+  test('Example-two test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  
+    // Click the get started link .
+    await page.getByRole('link', { name: 'Get started' }).click();
+  
+    // Expects the URL to contain intro.
+  });
+  test('Example-three test', async ({ page }) => {
+      await page.goto('https://playwright.dev/');
+    
+      // Expect a title "to contain" a substring.
+      await expect(page).toHaveTitle(/Playwright/);
+    });
+    
+   test('Example-four test', async ({ page }) => {
+      await page.goto('https://playwright.dev/');
+    
+      // Click the get started link .
+      await page.getByRole('link', { name: 'Get started' }).click();
+    
+      // Expects the URL to contain intro.
+    });
+    test('Example-five test', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+      
+        // Expect a title "to contain" a substring.
+        await expect(page).toHaveTitle(/Playwright/);
+      });
+      
+    test('Example-six test', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+      
+        // Click the get started link .
+        await page.getByRole('link', { name: 'Get started' }).click();
+      
+        // Expects the URL to contain intro.
+      }); 
 });
